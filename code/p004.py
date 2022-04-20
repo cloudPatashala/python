6db17098a395d651221b597f4d7fab2e5392e37d
Cloud <cloud.patashala@gmail.com>
2022-04-20 21:04:42 +0530

    Python input function

---
+++ code/p004.py
@@ -0,0 +1,12 @@
+# Welcome to Cloud Patashala Python Series.
+
+# Today we look at the python input() function.
+# This is useful when we have to take it input from the user at Run time.
+
+# We also would be looking at a tool called Thonny.
+# Which we can use to understand the execution flow.
+
+#print(input("What is your Name? "))
+
+print("Hello " + input("What is your Name? ") +
+      " welcome to Cloud Patashala's Python Series")
