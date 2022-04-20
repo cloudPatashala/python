78c69721097fc0cf4432d39067fc8e069c24e93a
Cloud <cloud.patashala@gmail.com>
2022-04-20 21:05:42 +0530

    Code Challange 1: Random Band Name Generator

---
+++ code/p005.py
@@ -0,0 +1,8 @@
+# Random Rock Band Name Generator.
+
+# In this program we will be using the input funtion to get City Name and Pet's Name from the user.
+# And using the information gathered we will be suggesting a Rock Band Name.
+
+
+print(input("Enter the name of a City: ") + " " +
+      input("Enter name of your Pet: ") + " could be a good suggestion for your band.")
