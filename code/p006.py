3e78df0c7d24a1b4186bcbb46f305ba8f9ffde16
Cloud <cloud.patashala@gmail.com>
2022-04-23 15:10:34 +0530

    python lenght funtionC

---
+++ code/p006.py
@@ -0,0 +1,8 @@
+# Today we look at the python Length function.
+
+# We will be using the input function to get the name of the person and then
+# calculate the number of characters in it.
+
+print(len(input("What is your first name: ")))
+
+print(len(input("What is your Last name: ")))
