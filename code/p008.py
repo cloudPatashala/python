93ad8f7c0cc5f3792f8b4629caecdd42ea7c1c0f
Cloud <cloud.patashala@gmail.com>
2022-04-23 15:11:58 +0530

    code challange 2

---
+++ code/p008.py
@@ -0,0 +1,24 @@
+# Code challange 2
+
+# Switch the values of the variables.
+# If
+# A = input(“a: “)
+# B = input(“b: “)
+
+# Value b should be printed with A
+# Value of b should be printed with A
+
+
+a = 5
+b = 10
+
+print("Value of a without interchange is " + str(a))
+print("Value of b without interchange is " + str(b))
+
+c = a
+a = b
+b = c
+
+print(c)
+print("Vaule of a after interchange is " + str(a))
+print("Value of b after interchange is " + str(b))
